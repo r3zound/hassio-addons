c3239ffb7ee582c33e8f298e0a8a64e8b2db81f8
Desmond <djhhui@gmail.com>
2024-11-12 13:31:03 +0800

    update build.yaml

--- WeChat/Wechat_Robot.py
+++ WeChat/Wechat_Robot.py
@@ -9,7 +9,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 # Read configuration from config.ini
 config = configparser.ConfigParser()
-config.read('hassio-addons/WeChat/config.ini')  # Updated path to config.ini
+config.read('config.ini')  # Updated path to config.ini
 
 # Ensure the required configuration keys are present
 TOKEN = config['DEFAULT'].get('TOKEN')
@@ -20,20 +20,24 @@ robot.config['ENCODING_AES_KEY'] = config['DEFAULT'].get('ENCODING_AES_KEY')
 api_key = config['DEFAULT'].get('ZhipuAI')
 
 client = ZhipuAI(api_key=api_key)
+#print (TOKEN, api_key)
+
 
 @robot.text
 def echo(message):
     response1 = client.chat.completions.create(
-        model="glm-4-Flash",
+        model="glm-4-plus",
         messages=[
+            {"role": "system", "content": "你是我微信公众号的智能助理，以纯文本的方式对话，简洁明了，不长篇大论。你的名字叫老王杂谈说。你尊重每个跟你对话的对象，并且多用礼貌用语"},
+            {"role": "user", "content": "我是一位老王杂谈说的一个读者"},
             {"role": "user", "content": message.content},
         ],
     )
-    
+    #return "hello"
     return response1.choices[0].message.content
 
-@robot.error_page
-def make_error_page(url):
-    return "<h1>喵喵喵 %s 不是给麻瓜访问的快走开</h1>" % url
+#@robot.error_page
+#def make_error_page(url):
+#    return "<h1>喵喵喵 %s 不是给麻瓜访问的快走开</h1>" % url
 
 robot.run()
