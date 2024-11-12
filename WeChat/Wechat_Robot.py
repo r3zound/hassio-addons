3524176a611b9dfee6ff7cd9d31ae6fc3ca849aa
Desmond <djhhui@gmail.com>
2024-11-12 19:15:21 +0800

    update docker-entrypoint.sh

--- WeChat/Wechat_Robot.py
+++ WeChat/Wechat_Robot.py
@@ -10,10 +10,10 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 # Fetch configuration from environment variables (Home Assistant injects them)
 TOKEN = os.getenv('TOKEN', 'WeRobot')  # Default to 'WeRobot' if not set
 HOST = os.getenv('HOST', '0.0.0.0')  # Default to '0.0.0.0' if not set
-APP_ID = os.getenv('APP_ID')  # Default to 'random' if not set
-ENCODING_AES_KEY = os.getenv('ENCODING_AES_KEY')  # Default to 'random'
-ZHIPUAI_KEY = os.getenv('ZHIPUAI_KEY')  # Get ZhipuAI key from environment
-PORT = os.getenv('PORT')  # Default to 8888 if not set
+APP_ID = os.getenv('APP_ID')  
+ENCODING_AES_KEY = os.getenv('ENCODING_AES_KEY')  
+ZHIPUAI_KEY = os.getenv('ZHIPUAI_KEY') 
+PORT = os.getenv('PORT')  
 data_path = os.getenv('data_path')  # Path for addon data
 
 # Initialize WeRoBot with the token from the config
@@ -21,6 +21,7 @@ robot = werobot.WeRoBot(token=TOKEN)
 robot.config['HOST'] = HOST
 robot.config['APP_ID'] = APP_ID
 robot.config['ENCODING_AES_KEY'] = ENCODING_AES_KEY
+robot.config['PORT'] = PORT
 
 # Initialize ZhipuAI and set the API key
 client = ZhipuAI(api_key=ZHIPUAI_KEY)
@@ -28,7 +29,7 @@ client = ZhipuAI(api_key=ZHIPUAI_KEY)
 @robot.text
 def echo(message):
     response1 = client.chat.completions.create(
-        model="glm-4-plus",
+        model="glm-4-flash",
         messages=[
             {"role": "system", "content": "你是我微信公众号的智能助理，以纯文本的方式对话，简洁明了，不长篇大论。你的名字叫老王杂谈说。你尊重每个跟你对话的对象，并且多用礼貌用语"},
             {"role": "user", "content": "我是一位老王杂谈说的一个读者"},
--- WeChat/docker-entrypoint.sh
+++ WeChat/docker-entrypoint.sh
@@ -8,7 +8,16 @@ if [ ! -f "/config/addons_config/wechat-server/Wechat_Robot.py" ]; then
 cp /Wechat_Robot.py /config/addons_config/wechat-server/Wechat_Robot.py
 fi
 
+echo "DATA_PATH=$data_path" > /config/addons_config/wechat-server/.env
+echo "TOKEN=$TOKEN" >> /config/addons_config/wechat-server/.env
+echo "HOST=$HOST" >> /config/addons_config/wechat-server/.env
+echo "APP_ID=$APP_ID" >> /config/addons_config/wechat-server/.env
+echo "ENCODING_AES_KEY=$ENCODING_AES_KEY" >> /config/addons_config/wechat-server/.env
+echo "ZHIPUAI_KEY=$ZHIPUAI_KEY" >> /config/addons_config/wechat-server/.env
+echo "PORT=$PORT" >> /config/addons_config/wechat-server/.env
 
+# 载入 .env 文件中的环境变量
+source /config/addons_config/wechat-server/.env
 
 python /config/addons_config/wechat-server/Wechat_Robot.py
 
