8a905ad39c1836eac737c47ca48d5f5928c85b62
Desmond <djhhui@gmail.com>
2024-11-12 15:57:40 +0800

    update dockerfile

--- WeChat/docker-entrypoint.sh
+++ WeChat/docker-entrypoint.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
 
-#uid=${UID:=$(id -u)}
-#gid=${GID:=$(id -g)}
-#admin_user=${ADMIN_USER:="admin"}
-#admin_password=${ADMIN_PASSWORD:="123456"}
-#domain=${DOMAIN:=""}
-#
-#if test ! -z $domain; then
-#  sed -i "s/# domain = .*\$/domain = \"$domain\"/ig" /home/wechat-server/config/config.toml
-#fi
-#
-#if grep -q "name = \"$admin_user\"" /home/wechat-server/config/config.toml; then
-#cat <<EOT >> /home/wechat-server/config/config.toml
-#[[user]]
-#name = "$admin_user"
-#password = "$admin_password"
-#email = "$admin_user@$domain"
-#is_admin = true
-#EOT
-#fi
+
 
 mkdir -p /config/addons_config/wechat-server 
 
