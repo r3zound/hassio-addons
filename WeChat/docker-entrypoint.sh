c72a0a9d333f80af20c2da82f5447d0139360a86
Desmond <djhhui@gmail.com>
2024-11-12 20:53:24 +0800

    update docker-entrypoint.sh

--- WeChat/docker-entrypoint.sh
+++ WeChat/docker-entrypoint.sh
@@ -1,21 +1,22 @@
-#!/usr/bin/with-contenv bashio
-
-# Export options from configuration as environment variables
-export DATA_PATH=$(bashio::config 'data_path')
-export TOKEN=$(bashio::config 'TOKEN')
-export HOST=$(bashio::config 'HOST')
-export APP_ID=$(bashio::config 'APP_ID')
-export ENCODING_AES_KEY=$(bashio::config 'ENCODING_AES_KEY')
-export ZHIPUAI_KEY=$(bashio::config 'ZhipuAI')
+#!/bin/sh
 
 
 
 mkdir -p /config/addons_config/wechat-server 
 cd /config/addons_config/wechat-server 
-if [ ! -f "/config/addons_config/wechat-server/Wechat_Robot.py" ]; then
-cp /Wechat_Robot.py /config/addons_config/wechat-server/Wechat_Robot.py
-fi
 
+cp /Wechat_Robot.py /config/addons_config/wechat-server/Wechat_Robot.py
+
+echo "DATA_PATH=$data_path" > /config/addons_config/wechat-server/.env
+echo "TOKEN=$TOKEN" >> /config/addons_config/wechat-server/.env
+echo "HOST=$HOST" >> /config/addons_config/wechat-server/.env
+echo "APP_ID=$APP_ID" >> /config/addons_config/wechat-server/.env
+echo "ENCODING_AES_KEY=$ENCODING_AES_KEY" >> /config/addons_config/wechat-server/.env
+echo "ZHIPUAI_KEY=$ZHIPUAI_KEY" >> /config/addons_config/wechat-server/.env
+echo "PORT=$PORT" >> /config/addons_config/wechat-server/.env
+
+# 载入 .env 文件中的环境变量
+source /config/addons_config/wechat-server/.env
 
 python /config/addons_config/wechat-server/Wechat_Robot.py
 
