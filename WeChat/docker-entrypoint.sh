77a2b8d9104c6e9765680c733229c095e054a380
Desmond <djhhui@gmail.com>
2024-12-10 13:13:41 +0800

    remove bashio

--- WeChat/docker-entrypoint.sh
+++ WeChat/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
-export TOKEN=$(bashio::config 'TOKEN')
-export HOST=$(bashio::config 'HOST')
-export APP_ID=$(bashio::config 'APP_ID')
-export APP_SECRET=$(bashio::config 'APP_SECRET')
-export ENCODING_AES_KEY=$(bashio::config 'ENCODING_AES_KEY')
-export ZhipuAI=$(bashio::config  'ZhipuAI')
+#export TOKEN=$(bashio::config 'TOKEN')
+#export HOST=$(bashio::config 'HOST')
+#export APP_ID=$(bashio::config 'APP_ID')
+#export APP_SECRET=$(bashio::config 'APP_SECRET')
+#export ENCODING_AES_KEY=$(bashio::config 'ENCODING_AES_KEY')
+#export ZhipuAI=$(bashio::config  'ZhipuAI')
 
 
 mkdir -p /config/addons_config/wechat-server 
