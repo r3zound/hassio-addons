b4436fa9a0488ec56b2ccc08354808d32cd0aa4e
Dede <djhhui@gmail.com>
2025-01-08 19:43:59 +0800

    更改启动方式

--- phddns/run.sh
+++ phddns/run.sh
@@ -1,9 +1,12 @@
-phddns enable
-phddns start
-sleep 10
-phddns status
+/usr/bin/phtunnel -l /var/log/phddns/phtunnel.log -c /data/phtunnel.json
 
-while true; do
-    sleep 60
-    phddns status
-done
+
+#phddns enable
+#phddns start
+#sleep 10
+#phddns status
+
+#while true; do
+#    sleep 60
+#    phddns status
+#done
