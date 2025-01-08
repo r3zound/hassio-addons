33653e8b37237825aaf8bd4e18ffae8feae68f2d
Dede <djhhui@gmail.com>
2025-01-08 21:40:57 +0800

    增加一分钟 后显示sn

--- phddns/run.sh
+++ phddns/run.sh
@@ -1,12 +1,13 @@
-/usr/bin/phtunnel -l /var/log/phddns/phtunnel.log -c /data/phtunnel.json
+/usr/bin/phtunnel -l /var/log/phddns/phtunnel.log -c /data/phtunnel.json -r -i 11429 -k adf9fb5a1ad438b9  -f 127.0.0.1:1101 &
+
 
 
 #phddns enable
 #phddns start
-#sleep 10
-#phddns status
+sleep 10
+phddns status
 
-#while true; do
-#    sleep 60
-#    phddns status
-#done
+while true; do
+    sleep 60
+    phddns status
+done
