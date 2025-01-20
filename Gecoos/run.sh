3154fd2f68db197bb539ce5612bc1f6191115b74
Dede <djhhui@gmail.com>
2025-01-20 19:26:31 +0800

    更新run.sh

--- Gecoos/run.sh
+++ Gecoos/run.sh
@@ -1,2 +1,3 @@
-
-/ac -p 60650 -mp 8080 -f /data/upload/ -dbpath /data -token 1 -lang zh -https 0 -isonlyoneprot 0
+API_PORT=$(jq -r '.api_port' /data/options.json)
+WEB_PORT=$(jq -r '.web_port' /data/options.json)
+/ac -p $API_PORT -mp $WEB_PORT -f /data/upload/ -dbpath /data -token 1 -lang zh -https 0 -isonlyoneprot 0
