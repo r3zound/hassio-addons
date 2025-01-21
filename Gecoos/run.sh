c81571ac0564b819c6443656004915e1e4b99d6a
Dede <djhhui@gmail.com>
2025-01-21 17:44:33 +0800

    更新配置

--- Gecoos/run.sh
+++ Gecoos/run.sh
@@ -1,3 +1,8 @@
 API_PORT=$(jq -r '.api_port' /data/options.json)
-WEB_PORT=$(jq -r '.web_port' /data/options.json)
-/ac -p $API_PORT -mp $WEB_PORT -f /data/upload/ -dbpath /data -token 1 -lang zh -https 0 -isonlyoneprot 0
+DEBUG=$(jq -r '.debug // 1' /data/options.json)
+TOKEN=$(jq -r '.token // 1' /data/options.json)
+LANG=$(jq -r '.lang // "zh"' /data/options.json)
+PASSWD=$(jq -r '.passwd // ""' /data/options.json)
+SHOWTIP=$(jq -r '.showtip // 1' /data/options.json)
+
+/ac -p $API_PORT -mp 8080 -f /data/upload/ -dbpath /data -token $TOKEN -lang $LANG -https 0 -isonlyoneprot 0 -debug $DEBUG -passwd "$PASSWD" -showtip $SHOWTIP
