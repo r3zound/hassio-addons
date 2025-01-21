dfe13fdb2da383413136c2fd947edc81d97cebba
Dede <djhhui@gmail.com>
2025-01-21 19:36:53 +0800

    移除rsync

--- OpenWrt/entrypoint.sh
+++ OpenWrt/entrypoint.sh
@@ -4,21 +4,21 @@
 # 只有在/data下没有对应目录时才执行初始化
 if [ ! -d "/data/etc" ]; then
     mkdir -p /data/etc
-    rsync -a /etc/ /data/etc/
+    cp -a /etc/ /data/etc/
     rm -rf /etc
     ln -s /data/etc /etc
 fi
 
 if [ ! -d "/data/var" ]; then
     mkdir -p /data/var
-    rsync -a /var/ /data/var/
+    cp -a /var/ /data/var/
     rm -rf /var
     ln -s /data/var /var
 fi
 
 if [ ! -d "/data/usr" ]; then
     mkdir -p /data/usr
-    rsync -a /usr/ /data/usr/
+    cp -a /usr/ /data/usr/
     rm -rf /usr
     ln -s /data/usr /usr
 fi
