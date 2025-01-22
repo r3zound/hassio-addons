2243f92b0eefd9da42e1cebd52e04b80ada44e91
Dede <djhhui@gmail.com>
2025-01-22 15:16:57 +0800

    尝试更改overlay

--- OpenWrt/entrypoint.sh
+++ OpenWrt/entrypoint.sh
@@ -1,22 +1,7 @@
-#!/bin/sh
-
-# 确保目录存在
-# 只有在/data下没有对应目录时才执行初始化
-if [ ! -d "/data/etc" ]; then
-    mkdir -p /data/etc
-    cp -a /etc/ /data/etc/
-    rm -rf /etc
-    ln -s /data/etc /etc
-fi
+#!/bin/ash
 
 
+/etc/init.d/overlay_setup start
+/sbin/init
 
-if [ ! -d "/data/usr" ]; then
-    mkdir -p /data/usr
-    cp -a /usr/ /data/usr/
-    rm -rf /usr
-    ln -s /data/usr /usr
-fi
-
-# 启动主进程
 
