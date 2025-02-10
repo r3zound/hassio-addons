a7e91020b58023cddb49f7eeafd42277e135a545
Dede <djhhui@gmail.com>
2025-02-10 20:05:56 +0800

    更新运行文件

--- k2fsa - edge/rootfs/k2fsa/init.sh
+++ k2fsa - edge/rootfs/k2fsa/init.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#/bin/bash
 
-if [ ! -f "/module/start.sh" ]; then
-    cp /k2fsa/start.sh "/module/start.sh"
-fi
+/k2fsa/bin/sherpa-onnx-offline-websocket-server \
+        --port=6006 \
+        --num-work-threads=5 \
+        --tokens=module/tokens.txt \
+        --encoder=module/encoder-epoch-12-avg-4.onnx \
+        --decoder=module/decoder-epoch-12-avg-4.onnx \
+        --joiner=module/joiner-epoch-12-avg-4.onnx \
+        --max-batch-size=5 \
+        --debug
 
-/module/start.sh
