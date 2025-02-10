6cf446934f9fda15b94ec0652f2e7e7f222a0f4a
Dede <djhhui@gmail.com>
2025-02-10 17:48:40 +0800

    缩减静像尺寸

--- k2fsa/rootfs/k2fsa/start.sh
+++ k2fsa/rootfs/k2fsa/start.sh
@@ -1,20 +1,6 @@
 #/bin/bash
 
-#https://k2-fsa.github.io/sherpa/onnx/websocket/offline-websocket.html#build-sherpa-onnx-with-websocket-support
-# 检查环境变量USE_INT8是否存在
-echo "Loading onnx, please wait 2 minutes..."
-if [ -n "$USE_INT8" ]; then
-    echo "use int8.onnx!"
-    /k2fsa/bin/sherpa-onnx-offline-websocket-server \
-        --port=10300 \
-        --num-work-threads=5 \
-        --tokens=/module/tokens.txt \
-        --encoder=/module/encoder-epoch-12-avg-4.int8.onnx \
-        --decoder=/module/decoder-epoch-12-avg-4.int8.onnx \
-        --joiner=/module/joiner-epoch-12-avg-4.int8.onnx \
-        --max-batch-size=5
-else
-    /k2fsa/bin/sherpa-onnx-offline-websocket-server \
+/k2fsa/bin/sherpa-onnx-offline-websocket-server \
         --port=10300 \
         --num-work-threads=5 \
         --tokens=/module/tokens.txt \
@@ -22,4 +8,4 @@ else
         --decoder=/module/decoder-epoch-12-avg-4.onnx \
         --joiner=/module/joiner-epoch-12-avg-4.onnx \
         --max-batch-size=5
-fi
+
