91d694d6da874cd5c325af13dc2f7cba671f2679
Dede <djhhui@gmail.com>
2025-02-10 20:04:04 +0800

    更改模型文件

--- k2fsa - edge/rootfs/k2fsa/start.sh
+++ k2fsa - edge/rootfs/k2fsa/start.sh
@@ -4,9 +4,9 @@
         --port=6006 \
         --num-work-threads=5 \
         --tokens=/module/tokens.txt \
-        --encoder=/module/encoder-epoch-12-avg-4.onnx \
-        --decoder=/module/decoder-epoch-12-avg-4.onnx \
-        --joiner=/module/joiner-epoch-12-avg-4.onnx \
+        --encoder=module/encoder-epoch-12-avg-4.onnx \
+        --decoder=module/decoder-epoch-12-avg-4.onnx \
+        --joiner=module/joiner-epoch-12-avg-4.onnx \
         --max-batch-size=5 \
         --debug
 
