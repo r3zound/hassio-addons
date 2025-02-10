cc017d13aa7edcda98e331adbdf8f9769d1dfe85
Dede <djhhui@gmail.com>
2025-02-10 22:48:26 +0800

    移除python版

--- k2fsa/rootfs/k2fsa/init.sh
+++ k2fsa/rootfs/k2fsa/init.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#/bin/bash
 
-python3 -m sherpa-onnx-offline-websocket-server \
-        --port=10300 \
+/k2fsa/bin/sherpa-onnx-offline-websocket-server \
+        --port=6006 \
         --num-work-threads=5 \
         --tokens=module/tokens.txt \
         --encoder=module/encoder-epoch-12-avg-4.onnx \
         --decoder=module/decoder-epoch-12-avg-4.onnx \
         --joiner=module/joiner-epoch-12-avg-4.onnx \
-        --max-batch-size=5 
+        --max-batch-size=5 \
+        --debug
 
