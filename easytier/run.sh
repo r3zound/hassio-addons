9d682618fad66c3a16079fda61e19e0dc41a8c85
Dede <djhhui@gmail.com>
2025-02-24 14:16:51 +0800

    添加缺失的DHCP

--- easytier/run.sh
+++ easytier/run.sh
@@ -11,11 +11,13 @@ network_name=$(jq -r '.easytier.network_name' /data/options.json)
 network_secret=$(jq -r '.easytier.network_secret' /data/options.json)
 uri=$(jq -r '.easytier.uri' /data/options.json)
 cidr=$(jq -r '.easytier.cidr' /data/options.json)
+dhcp=$(jq -r '.easytier.dhcp' /data/options.json)
 
 # 创建并写入 TOML 文件
 cat <<EOF > /data/config.yaml
 hostname = "$hostname"
 instance_name = "$instance_name"
+dhcp = $dhcp
 instance_id = "$instance_id"
 rpc_portal = "$rpc_portal"
 
